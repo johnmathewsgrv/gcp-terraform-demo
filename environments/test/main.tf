69093d47760f5a193bb9109e973ff3dd972f2d2f
johnmathewsgrv <johnmathewsgrv@gmail.com>
2026-04-10 19:12:38 +0530

    Update main.tf

--- environments/test/main.tf
+++ environments/test/main.tf
@@ -8,6 +8,8 @@ resource "google_storage_bucket" "demo_bucket" {
   location      = var.region
   force_destroy = true
 
+  uniform_bucket_level_access = true  
+
   labels = {
     environment = var.env
     managed_by  = "terraform"
