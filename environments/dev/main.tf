38334b9de31cd91b4dce3a91ffdf77fe78bc171a
johnmathewsgrv <johnmathewsgrv@gmail.com>
2026-04-10 19:03:25 +0530

    Create main.tf

---
+++ environments/dev/main.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "demo_bucket" {
+  name          = "${var.env}-demo-bucket-${var.project_id}"
+  location      = var.region
+  force_destroy = true
+
+  labels = {
+    environment = var.env
+    managed_by  = "terraform"
+  }
+}
